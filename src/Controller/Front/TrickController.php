833e5c799a8cacf6fc7c828737bc60b5c6cc5f50
TABET Damien <tabetdamien@free.fr>
2019-11-08 17:55:34 +0100

    Create comment adding functions

--- src/Controller/Front/TrickController.php
+++ src/Controller/Front/TrickController.php
@@ -2,20 +2,36 @@
 
 namespace App\Controller\Front;
 
+use App\Entity\Comment;
 use App\Entity\Trick;
+use App\Form\CommentType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class TrickController extends AbstractController
 {
+    public $user;
+
+    public function __construct(AuthenticationUtils $authenticationUtils, UserRepository $userRepository)
+    {
+        $this->user = $userRepository->findOneBy([
+            'email' => $authenticationUtils->getLastUsername()
+        ]);
+    }
+
     /**
      * @Route("trick/{id}-{slug}", name="trick.show", requirements={"slug": "[a-z0-9\-]*" })
      * @param Trick $trick
      * @param string $slug
+     * @param Request $request
      * @return Response
+     * @throws \Exception
      */
-    public function show(Trick $trick, string $slug)
+    public function show(Trick $trick, string $slug, Request $request)
     {
         if ($trick->getSlug() !== $slug) {
             $this->redirectToRoute('trick.show', [
@@ -23,9 +39,29 @@ class TrickController extends AbstractController
                 'slug' => $trick->getSlug(),
             ], 301);
         }
+
+        $comment = new Comment();
         $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $comment->setIdTrick($trick);
+            $comment->setIdUser($this->user);
+            $comment->setCreatedAt(new \DateTime());
+
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+            //$this->addFlash('success', 'Figure N°' . $trick->getId() . ' modifiée');
+
+            return $this->redirectToRoute('trick.show', ['id' => $trick->getId(), 'slug' => $trick->getSlug()]);
+        }
+
         return $this->render('front/trick/show.html.twig', [
-            'trick' => $trick
+            'trick' => $trick,
             'form' => $form->createView(),
             'comments' => $trick->getComments()
         ]);
