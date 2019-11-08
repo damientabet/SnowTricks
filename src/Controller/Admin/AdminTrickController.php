601bc334bccde2dbcc75074a50c38805fef30401
TABET Damien <tabetdamien@free.fr>
2019-11-08 11:28:44 +0100

    Create images adding function

--- src/Controller/Admin/AdminTrickController.php
+++ src/Controller/Admin/AdminTrickController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Image;
 use App\Entity\Trick;
-//use App\Entity\User;
 use App\Entity\Video;
+use App\Form\ImageType;
 use App\Form\TrickType;
 use App\Form\VideoType;
 use App\Repository\TrickRepository;
 use App\Repository\UserRepository;
+use App\Service\FileUploader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -77,16 +79,20 @@ class AdminTrickController extends AbstractController
      * @Route("admin/trick/edit/{id}", name="trick.edit")
      * @param Trick $trick
      * @param Request $request
+     * @param FileUploader $fileUploader
      * @return RedirectResponse|Response
      * @throws \Exception
      */
-    public function edit(Trick $trick, Request $request)
+    public function edit(Trick $trick, Request $request, FileUploader $fileUploader)
     {
         $video = new Video();
+        $image = new Image();
         $form = $this->createForm(TrickType::class, $trick);
         $form_video = $this->createForm(VideoType::class, $video);
+        $form_image = $this->createForm(ImageType::class, $image);
         $form->handleRequest($request);
         $form_video->handleRequest($request);
+        $form_image->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
 
@@ -115,11 +121,33 @@ class AdminTrickController extends AbstractController
             return $this->redirectToRoute('trick.edit', ['id' => $trick->getId()]);
         }
 
+        if ($form_image->isSubmitted() && $form_image->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $imageFile = $form_image['name']->getData();
+            if ($imageFile) {
+                $imageFileName = $fileUploader->upload($imageFile);
+                $image->setName($imageFileName);
+            }
+
+            $image->setIdTrick($trick);
+            $image->setCreatedAt(new \DateTime());
+
+            $entityManager->persist($image);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+            //$this->addFlash('success', 'Figure N°' . $trick->getId() . ' modifiée');
+
+            return $this->redirectToRoute('trick.edit', ['id' => $trick->getId()]);
+        }
+
         return $this->render('admin/trick/edit.html.twig', [
             'trick' => $trick,
             'videos' => $trick->getVideos(),
+            'images' => $trick->getImages(),
             'form' => $form->createView(),
             'form_video' => $form_video->createView(),
+            'form_image' => $form_image->createView(),
         ]);
     }
 
---
+++ src/Form/ImageType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Image;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ImageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', FileType::class, [
+                'mapped' => false,
+                'label' => 'Your files',
+                'multiple' => false
+            ])
+            ->add('active', CheckboxType::class, [
+                'required' => false
+            ])
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Image::class,
+        ]);
+    }
+}
