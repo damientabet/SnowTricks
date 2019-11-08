f2c76800351d7ae6dda092977fc8de94e4e7f086
TABET Damien <tabetdamien@free.fr>
2019-11-08 23:27:56 +0100

    Create comments + protect update function

--- src/Controller/Front/CommentController.php
+++ src/Controller/Front/CommentController.php
@@ -3,13 +3,53 @@
 namespace App\Controller\Front;
 
 use App\Entity\Comment;
+use App\Form\CommentType;
 use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController
 {
+    /**
+     * @Route("comment/edit/{id}", name="comment.edit")
+     * @param Comment $comment
+     * @param TrickRepository $trickRepository
+     * @param Request $request
+     * @return Response
+     */
+    public function edit(Comment $comment, TrickRepository $trickRepository, Request $request)
+    {
+        $user = $this->getUser();
+        $id_trick = $comment->getIdTrick();
+        $trick = $trickRepository->findOneBy(['id' => $id_trick]);
+
+        if ($user->getId() !== $comment->getIdUser()->getId()) {
+            return $this->redirectToRoute('trick.show', ['id' => $trick->getId(), 'slug' => $trick->getSlug()]);
+        }
+        
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+            //$this->addFlash('success', 'Figure N°' . $trick->getId() . ' modifiée');
+
+            return $this->redirectToRoute('trick.show', ['id' => $trick->getId(), 'slug' => $trick->getSlug()]);
+        }
+
+        return $this->render('front/trick/comment/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
     /**
      * @Route("comment/delete/{id}", name="comment.delete")
      * @param Comment $comment
