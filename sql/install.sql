471ffe3c9b8ff0fb71190b2861c00cfa5309458b
TABET Damien <tabetdamien@free.fr>
2019-12-19 14:38:48 +0100

    Update install sql script

--- sql/install.sql
+++ sql/install.sql
@@ -67,6 +67,16 @@ CREATE TABLE `trick_group` (
   `updated_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Déchargement des données de la table `trick_group`
+--
+
+INSERT INTO `trick_group` (`id`, `name`, `description`, `created_at`, `updated_at`) VALUES
+(1, 'Les grabs', 'Un grab consiste à attraper la planche avec la main pendant le saut. Le verbe anglais to grab signifie « attraper. »', '2019-12-19 10:58:19', '2019-12-19 10:58:19'),
+(2, 'Les rotations', 'On désigne par le mot « rotation » uniquement des rotations horizontales', '2019-12-19 10:58:47', '2019-12-19 10:58:47'),
+(3, 'Les flips', 'Un flip est une rotation verticale. On distingue les front flips, rotations en avant, et les back flips, rotations en arrière.', '2019-12-19 10:59:09', '2019-12-19 10:59:09'),
+(4, 'Les slides', 'Un slide consiste à glisser sur une barre de slide.', '2019-12-19 10:59:25', '2019-12-19 10:59:25');
+
 -- --------------------------------------------------------
 
 --
@@ -102,6 +112,14 @@ CREATE TABLE `image` (
   CONSTRAINT `FK_C53D045FE25A52BB` FOREIGN KEY (`id_trick_id`) REFERENCES `trick` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `roles`, `password`, `created_at`, `pseudo`, `secure_key`) VALUES
+(1, 'tabetdamien@free.fr', '[\"ROLE_ADMIN\"]', '$2y$13$Jd9jcdY0qcm.mj5nUHAlcuZbJbOEDX05ssqnLDnL63g81SQzi3VFq', '2019-12-19 10:55:48', 'Dams', '2098d988f51f6ec0d7b2418b9f010477'),
+(2, 'test@test.fr', '[\"ROLE_USER\"]', '$2y$13$I9fgKT99MdtVHwCwUYNnB.hVSgzUbPfktga3rRL.VtmaoU6dzDoAu', '2019-12-19 13:29:21', 'Utilisateur test', '82b0f857c5b8db36ad5032c10fe7b6eb');
+
 -- --------------------------------------------------------
 
 --
@@ -119,6 +137,23 @@ CREATE TABLE `video` (
   CONSTRAINT `FK_7CC7DA2CE25A52BB` FOREIGN KEY (`id_trick_id`) REFERENCES `trick` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Déchargement des données de la table `video`
+--
+
+INSERT INTO `video` (`id`, `id_trick_id`, `name`, `created_at`, `active`, `url`, `type`) VALUES
+(1, 1, 'La liste des grabs', '2019-12-19 11:09:12', 1, 'https://www.youtube.com/embed/CA5bURVJ5zk', '1'),
+(2, 1, 'La session', '2019-12-19 11:10:13', 1, 'https://www.youtube.com/embed/Opg5g4zsiGY', '1'),
+(3, 2, 'Comment faire un Indy ?', '2019-12-19 11:15:35', 1, 'https://www.youtube.com/embed/iKkhKekZNQ8', '1'),
+(4, 2, 'Les grabs', '2019-12-19 11:16:13', 1, 'https://www.youtube.com/embed/CA5bURVJ5zk', '1'),
+(5, 3, 'Comment le faire ?', '2019-12-19 11:20:37', 1, 'https://www.youtube.com/embed/JMS2PGAFMcE', '1'),
+(6, 3, 'Les différentes rotations', '2019-12-19 11:21:16', 1, 'https://www.youtube.com/embed/6gFsbU3GWF0', '1'),
+(7, 4, 'Le 1080°', '2019-12-19 11:29:51', 1, 'https://www.youtube.com/embed/KWWIwmkqrDA', '1'),
+(8, 5, 'Frontflip', '2019-12-19 11:34:06', 1, 'https://www.youtube.com/embed/xhvqu2XBvI0', '1'),
+(9, 5, 'Backflip', '2019-12-19 11:35:15', 1, 'https://www.youtube.com/embed/arzLq-47QFA', '1'),
+(10, 6, 'Slides', '2019-12-19 13:21:50', 1, 'https://www.youtube.com/embed/R3OG9rNDIcs', '1'),
+(11, 6, 'Box slide', '2019-12-19 13:23:03', 1, 'https://www.youtube.com/embed/WOgw5uBSLp0', '1');
+
 -- --------------------------------------------------------
 
 --
@@ -134,3 +169,13 @@ CREATE TABLE `comment` (
   CONSTRAINT `FK_9474526C79F37AE5` FOREIGN KEY (`id_user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `FK_9474526CE25A52BB` FOREIGN KEY (`id_trick_id`) REFERENCES `trick` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `comment`
+--
+
+INSERT INTO `comment` (`id`, `id_user_id`, `id_trick_id`, `content`, `created_at`) VALUES
+(1, 1, 4, 'La figure la plus compliqué à mon avis !', '2019-12-19 13:28:10'),
+(2, 2, 1, 'Cette figure n\'est pas facile, mais j\'adore la pratiquer !', '2019-12-19 13:30:16'),
+(3, 2, 6, 'En réussissant ce genre de figure, nous ne sommes plus un débutant :-)', '2019-12-19 13:31:40'),
+(4, 1, 4, 'Je suis tout à fais d\'accord avec vous. Un tour, c\'est facile ; ensuite, c\'est une autre histoire ^^', '2019-12-19 13:32:53');
