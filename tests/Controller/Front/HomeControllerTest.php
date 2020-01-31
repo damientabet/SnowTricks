37b8be3b416693bcf12eae018f2ab5a8dedf7350
TABET Damien <tabetdamien@free.fr>
2020-01-31 10:54:43 +0100

    Create and update unit tests

--- tests/Controller/Front/HomeControllerTest.php
+++ tests/Controller/Front/HomeControllerTest.php
@@ -2,10 +2,17 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\Trick;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HomeControllerTest extends WebTestCase
 {
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
     public function testIndexPageIsSuccessful()
     {
         $client = static::createClient();
@@ -13,9 +20,47 @@ class HomeControllerTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 
+    protected function setUp(): void
+    {
+        $kernel = self::bootKernel();
+
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
     public function testIndexTrick()
     {
-        $tricks = [];
+        $tricks = $this->entityManager
+            ->getRepository(Trick::class)
+            ->findAll()
+        ;
         $this->assertIsArray($tricks);
+    }
+
+    public function testTrickName()
+    {
+        $trick = $this->entityManager
+            ->getRepository(Trick::class)
+            ->findOneBy(['id' => 1])
+        ;
+        $this->assertSame('Mute', $trick->getName());
+    }
+
+    public function testTrickIsActive()
+    {
+        $trick = $this->entityManager
+            ->getRepository(Trick::class)
+            ->findOneBy(['id' => 1])
+        ;
+        $this->assertTrue($trick->getActive());
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->entityManager->close();
+        $this->entityManager = null;
     }
 }
