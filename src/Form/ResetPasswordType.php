fdef308abe558c651889ea35c0bafba5be734e67
TABET Damien <tabetdamien@free.fr>
2020-02-14 16:36:48 +0100

    Update reset password form and display errors

--- src/Form/ResetPasswordType.php
+++ src/Form/ResetPasswordType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,13 +13,14 @@ class ResetPasswordType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('password', PasswordType::class, ['label' => 'Mot de passe'])
-            ->add('password_confirm', PasswordType::class, ['label' => 'Confirmation de mot de passe'])
-            ->add('submit', SubmitType::class, [
-                'label' => 'Réinitialiser'
-            ])
-        ;
+        $builder->add('password', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'invalid_message' => 'Les mots de passe doivent être identiques',
+            'options' => ['attr' => ['class' => 'mb-3 rounded-0 p-4']],
+            'required' => true,
+            'first_options'  => ['label' => 'Mot de passe'],
+            'second_options' => ['label' => 'Confirmation de mot de passe'],
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
