eb39e01b52176ac24295f4af865546c44c605256
TABET Damien <tabetdamien@free.fr>
2020-02-28 18:40:13 +0100

    Create User Service

--- src/Controller/Front/RegistrationController.php
+++ src/Controller/Front/RegistrationController.php
@@ -4,39 +4,28 @@ namespace App\Controller\Front;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
-use Exception;
+use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistrationController extends AbstractController
 {
     /**
      * @Route("/register", name="register")
      * @param Request $request
-     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param UserService $userService
      * @return Response
-     * @throws Exception
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function register(Request $request, UserService $userService): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $user->setPassword($passwordEncoder->encodePassword($user, $form->get('plainPassword')->getData()));
-            $user->setCreatedAt(new \DateTime());
-            $user->setRoles(['ROLE_USER']);
-            $user->setSecureKey(bin2hex(random_bytes(16)));
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
-            $this->addFlash('success', 'Votre compte à été crée');
-            return $this->redirectToRoute('login');
+            $userService->addUser($user, $form->get('plainPassword')->getData());
         }
 
         return $this->render('front/registration/register.html.twig', [
